2a794d57ade6944b3c863d229f1e69d3af38fb58
morty-lab <aniejosephcabahug@gmail.com>
2023-12-20 14:01:15 +0800

    commit suicide

--- app/src/main/java/com/example/loginandsignup/Homepage.java
+++ app/src/main/java/com/example/loginandsignup/Homepage.java
@@ -9,7 +9,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+<<<<<<< HEAD
 import android.widget.TextView;
+=======
+import android.widget.Button;
+>>>>>>> origin/master
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,6 +22,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+<<<<<<< HEAD
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,13 +34,25 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener 
 
     ArrayList<Note> notes ;
     DatabaseReference notereference;
+=======
+>>>>>>> origin/master
 
+import java.util.ArrayList;
+
+public class Homepage extends AppCompatActivity implements View.OnClickListener {
+
+    RecyclerView recycler;
+    ArrayList<Book> books;
+    DatabaseReference bookreference;
+    BookAdapter adapter;
+    FloatingActionButton floating_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
+<<<<<<< HEAD
         text = findViewById(R.id.text);
         fab = findViewById(R.id.fab);
         fab.setOnClickListener(this);
@@ -64,12 +81,44 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener 
                     }
 
                     else{
+=======
+        recycler = findViewById(R.id.bookRecycler);
+
+        bookreference = FirebaseDatabase.getInstance().getReference("books");
+        books = new ArrayList<>();
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new BookAdapter(this,books);
+        recycler.setAdapter(adapter);
+
+        floating_add = findViewById(R.id.FloatAddBook);
+        floating_add.setOnClickListener(this);
+
+        bookreference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                books.clear();
+                try {
+                    if (snapshot.exists()) {
+                        for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                            Book book = dataSnapshot.getValue(Book.class);
+                            books.add(book);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                    else {
+                        // Handle the case where no data exists
+                        // For example, you could show a message to the user
+>>>>>>> origin/master
                         Toast.makeText(getApplicationContext(), "No data found", Toast.LENGTH_SHORT).show();
                     }
                 } catch (Exception e){
                     Log.e("Fucking error", "Failed to fetch data", e);
                 }
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> origin/master
             }
 
             @Override
@@ -77,14 +126,18 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener 
 
             }
         });
+<<<<<<< HEAD
 
         for (Note element : notes) {
             Log.d("ArrayListContents", element.getnoteContent());
         }
+=======
+>>>>>>> origin/master
     }
 
     @Override
     public void onClick(View v) {
+<<<<<<< HEAD
         Intent intent = new Intent(this,AddNote.class);
         startActivity(intent);
 
@@ -94,5 +147,11 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener 
             Log.d("NoteManager", "Note title: " + note.getnoteTitle());
 
         }
+=======
+        Intent intent = new Intent(this,AddBook.class);
+        startActivity(intent);
+
+
+>>>>>>> origin/master
     }
 }
